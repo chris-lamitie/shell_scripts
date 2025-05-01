96f58330e24066fc9820b069206e1346f768e3c7
chris-lamitie <52215205+chris-lamitie@users.noreply.github.com>
2025-05-01 13:02:25 -0500

    Create top20save.sh

---
+++ top20save.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/bash
+########################################################
+# topsave.sh
+#
+# Collects topdata to a file. It should be
+# run from cron every minute. Saves to /var/tmp/topsave/
+# and makes day of week dir that only root can access.
+# Saves only the top header and first 20 lines of top
+# output.
+#
+# * * * * * /root/bin/topsave
+########################################################
+#variables
+STATE_OK=0
+STATE_CRITICAL=1
+GROUP="root"
+VER=`date +"%H%M"`
+DAY=`date +"%A"`
+TARGET="/var/tmp/topsave"
+
+###############################################
+# function for error checking in a shell      #
+# script                                      #
+###############################################
+function chkerr_exit {
+     $@
+     if [ $? -ne 0 ]
+     then
+        # epic fail.
+        echo "failed executing $@ "
+        exit $STATE_CRITICAL
+     fi
+}
+
+
+
+###############################################
+# ok, lets chek our variables
+###############################################
+if [ -z $TARGET ]
+then
+    echo "TARGET not set .. exiting"
+        exit $STATE_CRITICAL
+fi
+
+if [ -z $DAY ]
+then
+    echo "DAY not set .. exiting"
+        exit $STATE_CRITICAL
+fi
+
+if [ ! -d $TARGET/$DAY ]
+then
+    chkerr_exit "mkdir -p $TARGET/$DAY"
+        chkerr_exit "chgrp $GROUP  $TARGET/$DAY"
+        chkerr_exit "chmod 770 $TARGET/$DAY"
+fi
+
+
+
+if [ $VER -eq "0000" ]
+then
+    chkerr_exit "rm -rf $TARGET/$DAY/*"
+fi
+
+/usr/bin/top -b -n 1 | head -n 27 > $TARGET/$DAY/$VER
+
+exit $STATE_OK
