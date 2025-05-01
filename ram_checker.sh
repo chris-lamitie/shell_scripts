f017f09e12cc639a7978e79cce9e4665a1a85a57
chris-lamitie <52215205+chris-lamitie@users.noreply.github.com>
2025-05-01 13:32:49 -0500

    Update ram_checker.sh
    Minor output tweaks

--- ram_checker.sh
+++ ram_checker.sh
@@ -11,7 +11,7 @@ SECOND=`date +%S`
 my_uid=$(echo $EUID)
 used_swap=$(cat /proc/swaps | awk 'NR>1 {swap_sum+=$4} END {print swap_sum}')
 spacer="==============================="
- 
+
 echo "Running ram_and_cpu_check.sh at $YEAR-$MONTH-$DAY AT $HOUR:$MINUTE:$SECOND"
 if [ $my_uid -ne 0 ]
 then
@@ -19,42 +19,46 @@ then
 else
         echo -e "$spacer\n"
 fi
- 
+
 free -m | awk 'NR==2{printf "Current Memory Usage:       %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
 free -m | awk 'NR==2{printf "Available Memory:           %s/%sMB (%.2f%%)\n", $7,$2,$7*100/$2 }'
 free -m | awk 'NR==3 {if ($2 > 0) {printf "Current Swap Usage:          %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2} else {printf "Current Swap Usage:         0/0MB (0.00%%)\n"}}'
 free -m | awk 'NR==2{printf "Current buffer/cache Usage: %s/%sMB (%.2f%%)\n", $6,$3,$6*100/$3 }'
- 
+
 # This df command will show disk usage of the root (/) partition
 #df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}'i
- 
+
 # now we check inf smem is installed, and if so, use to to display things
 smem_installed=$(which smem > /dev/null 2>&1)
 #echo "smem_installed exit code: $?"
+#echo -e "used_swap: $used_swap"
+
 if [ $? -eq 0 ]
 then
         smem_ram_by_user=$(smem -uk | column -t)
         echo -e "\nsmem ram use by user:\n$spacer\n$smem_ram_by_user\n$spacer"
- 
+
 # uncomment the section below if you want to see gory details about swap through smem
 #       if [ $my_uid -eq 0 ] && [ $used_swap -gt 0 ] # we are root and we are swapping,do more with smem!
 #       then
 #               smem_totals_sorted_swap=$(smem -tkrs swap | awk 'NR <= 41 {print}')
 #               echo -e "\nTop 40 processes using swap:\n$spacer\n$smem_totals_sorted_swap\n$spacer"
 #       fi
- 
+
 else
         echo -e "\nsmem is not installed. Consider installing it for more output\n$spacer"
 fi
- 
+
 # now we are going to get top stats and do things with the outupt
 # we could simply dump the top 20 lines of top, but this exercise
 # came of learning to do things with awk to format output.
-top_output=$(top -bn1 | head -n 20)
+#top_output=$(top -bn1 | head -n 20)
+top_output=$(top -bn1 -o %MEM | head -n 20) # Sorts top by %Mem
 top_load_average=$(echo "$top_output" | grep load | awk '{printf "CPU 5 min. Load: %.2f\n", $(NF-2)}')
 #top_memory_stats=$(echo "$top_output" | grep -E "Mem|Swap")
 top_process_stats=$(echo "$top_output" | awk '/^ *PID/ {p=1} p && p++')
-echo "top stats:"
-echo "$top_load_average"
-#echo "$top_memory_stats"
-echo "$top_process_stats"
+echo ""
+echo -e "Top stats:\n$spacer"
+echo -e "Load Average: $top_load_average"
+#echo -e "Top Mem Stats:\n$top_memory_stats"
+echo -e "Process Stats:\n$top_process_stats"
